5731285f00769281237d8860be0dc68c8cc5b166
ShadowShark99 <kadenwchan@gmail.com>
2025-08-13 17:47:06 -0500

    Load Default Messages

--- app.js
+++ app.js
@@ -1,8 +1,37 @@
 const express = require("express");
+const indexRouter = require("./routers/indexRouter");
+const path = require("path");
 const app = express();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
+// app.get("/", (req, res) => {
+//   res.send("Message Board");
+// });
+
+//app.use("/", indexRouter);
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+//call middleware
 app.get("/", (req, res) => {
-  res.send("Message Board");
+  res.render("index", { title: "Mini Message Board", messages: messages });
+});
+
+app.get("/new", (req, res) => {
+  res.render("form");
 });
 
 const PORT = process.env.PORT || 3000;
---
+++ routers/indexRouter.js
@@ -0,0 +1,25 @@
+const { Router } = require("express");
+const path = require("path");
+const indexRouter = Router();
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
+//views
+
+//call middleware
+indexRouter.get("/", (req, res) => {
+  res.render("index", { messages: messages });
+});
+
+module.exports = indexRouter;
