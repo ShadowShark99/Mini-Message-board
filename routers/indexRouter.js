d8e78bc78091811dee135839261225a85f2d1a81
ShadowShark99 <kadenwchan@gmail.com>
2025-08-13 19:07:31 -0500

    Load single message

--- routers/indexRouter.js
+++ routers/indexRouter.js
@@ -7,5 +7,14 @@ const db = require("../db");
 indexRouter.get("/", (req, res) => {
   res.render("index", { title: "Mini Message Board", messages: db.messages });
 });
+indexRouter.post("/message", (req, res) => {
+  console.log("Post");
+  const i = parseInt(req.body.index, 10);
+  const message = db.messages[i];
+  if (!message) {
+    return res.status(404).send("Message not found");
+  }
+  res.render("message", { message });
+});
 
 module.exports = indexRouter;
